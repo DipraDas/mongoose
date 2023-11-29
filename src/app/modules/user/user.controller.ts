3f430f4abd65be31358d1eebe6db28d45aef1e64
Dipra <dipradas5940@gmail.com>
2023-11-30 00:17:56 +0600

    random

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,7 +1,7 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import { createUserToDb } from "./user.service";
 
-export const createUser = async (req: Request, res: Response, next: NextFunction) => {
+export const createUser = async (req: Request, res: Response) => {
     const user = await createUserToDb();
     res.status(200).json({
         status: 'success',
