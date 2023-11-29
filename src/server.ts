113de5b694fe58364bc28660392fa8e341681dbd
Dipra <dipradas5940@gmail.com>
2023-11-29 21:12:23 +0600

    6.3

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,18 @@
-const express = require('express')
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+import app from './app';
 
-const app = express()
-const port = 5300
+const port: number = 5300
 
 async function main() {
-
     try {
         await mongoose.connect('mongodb://127.0.0.1:27017/mongoose-practise');
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`)
+        })
     }
     catch (err) {
         console.log('Errror here', err)
     }
-    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
 main()
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
