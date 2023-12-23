02706c80b6f602519ee9bee56496d6b1dcf129fc
Dipra <dipradas5940@gmail.com>
2023-12-23 12:56:06 +0600

    8.8

--- mod-8.js
+++ mod-8.js
@@ -139,3 +139,25 @@ db.practice.aggregate([
         }
     }
 ])
+
+// ! 8.8
+
+// * Multi stage pipeline
+db.practice.aggregate([
+    {
+        $match: { _id: ObjectId('6406ad63fc13ae5a40000064') }
+    },
+    {
+        $facet: {
+            'friendsCount': [
+                { $project: { friendsCount: { $size: '$friends' } } }
+            ],
+            'interestsCount': [
+                { $project: { interestsCount: { $size: '$interests' } } }
+            ],
+            'skillsCount': [
+                { $project: { skillsCount: { $size: '$skills' } } }
+            ],
+        }
+    }
+])
